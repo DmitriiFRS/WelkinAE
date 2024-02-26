45ac349f4e536236a52733f0feea36332fbe50a9
Дмитрий <Dmitriifrs@gmail.com>
2024-02-26 11:40:15 +0500

    fix

--- app/pages/api/route.ts
+++ app/pages/api/route.ts
@@ -12,6 +12,7 @@ export async function POST(request: NextRequest) {
          service: "gmail",
          host: "smtp.gmail.com",
          port: 465,
+         secure: true,
          auth: {
             user: email,
             pass: pass,
